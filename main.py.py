74ba16682bbab3e68e170ee5108d215d1be638af
Nicoleta <nicoleta_flori@yahoo.com>
2023-03-30 10:59:37 -0700

    save

---
+++ main.py.py
@@ -0,0 +1,15 @@
+
+text = 'geeks for geeks'
+
+print(text.split())
+
+print(text.split( "  " ))
+
+word = 'geeks,for,geeks'
+print(word.split(","))
+
+x = 'geeks:for:geeks'
+print(x.split(':'))
+
+x = ',CatBatSatFatOr,'
+print(x.strip().split(" t "))
